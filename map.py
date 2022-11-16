b40221efd25c76d952309663cee3cc6b0b1d4d12
Abylkasymov <75130412+abylkasymoverbolat@users.noreply.github.com>
2022-11-16 16:41:15 +0600

    Update map.py

--- map.py
+++ map.py
@@ -1,3 +1,14 @@
+[15:31, 16.11.2022] Abylkasymov: import random
+names = ['Маша', 'Петя', 'Вася']
+code_names = ['Шпунтик', 'Винтик', 'Фунтик']
+for i in range(len(names)):
+names[i] = random.choice(code_names)
+print(names
+[15:43, 16.11.2022] Abylkasymov: import random
+names = ['Маша', 'Петя', 'Вася']
+secret_names = map(lambda x: random.choice(['Шпунтик', 'Винтик', 
+'Фунтик']), names)
+print(list(secret_names))
 
 
 
@@ -5,8 +16,7 @@
 
 
 
-
-
+Пример: list1 = [10, 11, 12, 13, 14] list2 = [20, 30, 42] print("List1 before Concatenation:\n" + str(list1)) for x in list2 : list1.append(x) print ("Concatenated list i.e. list1 after concatenation:\n" + str(list1))
 
 
 
